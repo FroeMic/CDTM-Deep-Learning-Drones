d64f3b7df174e300dabbbbee1a3c1e2b1cff4038
Michael Fröhlich <M.Froehlich@tum.de>
2016-08-12 00:02:49 +0200

    Adapt navData demo

--- src/demo/navData.py
+++ src/demo/navData.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
     drone.reset()                                      # Sets drone's status to good
     while (drone.getBattery()[0]==-1): time.sleep(0.1) # Wait until drone has done its reset
     print "Battery: "+str(drone.getBattery()[0])+"% "+str(drone.getBattery()[1]) # Battery-status
-    drone.useDemoMode(False)                           # Give me everything...fast
-    drone.getNDpackage(["demo","pressure_raw","altitude","magneto","wifi"]) # Packets to decoded
+    drone.useDemoMode(False)                        # Give me everything...fast
+    drone.setConfigAllID()
+    drone.getNDpackage(["demo","pressure_raw","altitude","magneto","wifi","raw_measures","gyros_offsets",]) # Packets to decoded
     time.sleep(0.5)                                    # Give it some time to awake fully after reset
 
     ##### Mainprogram begin #####
@@ -26,3 +27,5 @@ if __name__ == '__main__':
               +str(drone.State[21])+" / "+str(drone.NavData["pressure_raw"][0])
         print "Megnetometer [X,Y,Z]:         "+str(drone.NavData["magneto"][0])
         print "Wifi link quality:            "+str(drone.NavData["wifi"])
+        print "RawMeasures [X,Y,Z]:          "+str(drone.NavData["raw_measures"][0])
+        print "Gyros Offset [X,Y,Z]:         "+str(drone.NavData["gyros_offsets"])
