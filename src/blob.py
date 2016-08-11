45de9994e24f525c807cd2b8b2328ed84031b177
Laurenz <github@laure.nz>
2016-08-11 11:44:33 +0200

    Add ratio check in blob.py

--- src/blob.py
+++ src/blob.py
@@ -8,7 +8,7 @@ import math
 from matplotlib import pyplot as plt
 
 def main(argv):
-    file = 'test.png'
+    file = 'test3.png'
     if len(argv) != 0:
         file = argv[0]
 
@@ -17,14 +17,21 @@ def main(argv):
     #while cv2.waitKey(20) & 0xFF != ord("q"):
     if True:
         img = cv2.imread(file)
+
+        cv2.imshow('img', img)
+
         hsl = cv2.cvtColor(img, cv2.COLOR_RGB2HLS_FULL)[:,:,1]
+        cv2.imshow('hls', hsl)
 
         # enhance contrast
         hsl = clahe.apply(hsl)
+        cv2.imshow('clahe', hsl)
 
         # binary image
         blur = cv2.GaussianBlur(hsl, (9,9), 100)
+        cv2.imshow('blur', blur)
         flag, thresh = cv2.threshold(blur,200,255,cv2.THRESH_BINARY)
+        cv2.imshow('thresh', thresh)
 
         #get biggest contour
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -45,10 +52,25 @@ def main(argv):
         thresh_annot = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
         cv2.drawContours(thresh_annot, [box], -1, (0,0,255), 1)
 
+        # TODO: check ratio of rectangle, it must be roughly A4
+        a = math.hypot(box[0][0] - box[1][0], box[0][1] - box[1][1])
+        b = math.hypot(box[1][0] - box[2][0], box[1][1] - box[2][1])
+
+        ratio = a/b if a<b else b/a
+        correct_ratio = 210.0/297.0
+        ratio_deviation = abs((ratio-correct_ratio) / correct_ratio)
+
+        if ratio_deviation > 0.05:
+            print "ratio_deviation is above 5% ({})".format(100 * ratio_deviation)
+
         # Get biggest contour
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
         #cv2.drawContours(thresh_annot, contour, -1, (255,0,0), cv2.CV_AA)
+
+        # TODO: get inner circle shape
+
+        # TODO: get innner circle shape circularity moment
 
         # Fit a line (to get angle)
         rows,cols = thresh.shape[:2]
@@ -66,7 +88,7 @@ def main(argv):
 
         print "angle: {}, position: {}, {}".format(angle, X, Y)
 
-        cv2.imshow('thresh', thresh_annot)
+        cv2.imshow('info', thresh_annot)
         #cv2.imshow('img', img)
 
         cv2.waitKey(0)
