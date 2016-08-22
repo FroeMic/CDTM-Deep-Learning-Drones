d3ddb73b6f730c7c01331bc4e435b227e4542c7b
Michael Fröhlich <M.Froehlich1994@gmail.com>
2016-08-22 22:32:45 +0200

    Repository Cleanup

--- src/haar-classifier/start_training.sh
+++
@@ -1,1 +0,0 @@
-opencv_traincascade -data classifier -vec samples/aggregated.vec -bg negatives.txt -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 1000 -numNeg 597 -w 80 -h 40 -mode ALL -precalcValBufSize 1024 -precalcIdxBufSize 1024
